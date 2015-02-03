8c3527804b21d48dde641631037d8abb2df75450
Charlie Tuider <ctuider@siu.edu>
2015-02-03 15:13:26 -0600

    Update Charlie.py
    Added Fibonnaci challenge

--- Set_1/Charlie.py
+++ Set_1/Charlie.py
@@ -1,6 +1,7 @@
-#Programming Challenge 1 (FizzBuzz) 
+#Programming Challenge 1 (FizzBuzz, Fibonacci) 
 #Made by: justcharlie
 
+# Start FizzBuzz
 for num in range(1, 101):
 	if (num % 3 == 0 and num % 5 == 0):
 		print "FizzBuzz"
@@ -10,3 +11,16 @@ for num in range(1, 101):
 		print "Buzz"
 	else:
 		print num
+#End FizzBuzz
+
+#Start Fibonacci
+def Fib(num):
+    if (num == 0):
+        return 0
+    elif (num == 1):
+        return 1
+    else:
+        return (Fib(num-1)) + (Fib(num-2))
+
+print Fib(int(raw_input("Enter the index for a value in the Fibonacci sequence: ")))
+#End Fibonnaci
